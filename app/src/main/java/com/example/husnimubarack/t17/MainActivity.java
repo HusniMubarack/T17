250f05e09a669bd99a4514b6809f8830bd7876d9
Husni Mubarack <mhmubarack@gmail.com>
2017-08-30 03:11:35 +0530

    drawer as in the material design app

--- app/src/main/java/com/example/husnimubarack/t17/MainActivity.java
+++ app/src/main/java/com/example/husnimubarack/t17/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.husnimubarack.t17;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
-    private ArrayList<DrawerListItem> mDrawerItems= new ArrayList<>();
+    private ArrayList<DrawerListItem> mDrawerItems = new ArrayList<>();
     private ListView mDrawerList;
     private DrawerAdapter mDrawerAdapter;
 
@@ -24,17 +29,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Events"));
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Workshops"));
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Lectures"));
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Exhibitions"));
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Proshows"));
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Adizya"));
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Wheels"));
-        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man,"Blitzkrieg"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Events"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Workshops"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Lectures"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Exhibitions"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Proshows"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Adizya"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Wheels"));
+        mDrawerItems.add(new DrawerListItem(R.drawable.tathva_man, "Blitzkrieg"));
 
-        mDrawerAdapter = new DrawerAdapter(this,mDrawerItems);
-        mDrawerList=(ListView)findViewById(R.id.drawer_list);
+        mDrawerAdapter = new DrawerAdapter(this, mDrawerItems);
+        mDrawerList = (ListView) findViewById(R.id.drawer_list);
 
         mDrawerList.setAdapter(mDrawerAdapter);
 
@@ -43,20 +48,72 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("tathva '17");
         getSupportActionBar().setIcon(R.drawable.tathva_man);
 
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        mToggle =new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ListView navigationListView = (ListView) findViewById(R.id.drawer_list);
+
+        navigationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fragment fragment = null;
+                switch (position) {
+                    case 0:
+                        getSupportActionBar().setTitle("Events");
+                        fragment = new EventsFrag();
+                        break;
+                    case 1:
+                        fragment = new WorkshopsFrag();
+                        getSupportActionBar().setTitle("Workshops");
+                        break;
+                    case 2:
+                        fragment = new LecturesFrag();
+                        getSupportActionBar().setTitle("Lectures");
+                        break;
+                    case 3:
+                        fragment = new ExhibitionsFrag();
+                        getSupportActionBar().setTitle("Exhibitions");
+                        break;
+                    case 4:
+                        fragment = new ProshowsFrag();
+                        getSupportActionBar().setTitle("Proshows");
+                        break;
+                    case 5:
+                        fragment = new AdizyaFrag();
+                        getSupportActionBar().setTitle("Adizya");
+                        break;
+                    case 6:
+                        fragment = new WheelsFrag();
+                        getSupportActionBar().setTitle("Wheels");
+                        break;
+                    case 7:
+                        fragment = new BlitzkriegFrag();
+                        getSupportActionBar().setTitle("Blitzkrieg");
+                        break;
+                }
+                if (fragment != null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content_frame, fragment);
+                    ft.commit();
+                }
+
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(mToggle.onOptionsItemSelected(item)){
+        if (mToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
